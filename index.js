b5f77782dd3c265599f90284517dd83403db84cc
flip <ansuz@transitiontech.ca>
2014-10-14 21:01:58 -0400

    stuff happenin'

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express(),
+    options = {
+      debug:true
+      ,address:""
+      ,port:8086
+      ,alert:function(){console.log("Server listening on %s:%s",options.address,options.port)}
+    };
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname+"/public/index.html");
+});
+
+app.listen(options.port, options.address, options.alert);
