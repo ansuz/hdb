0e82a80507c56a1cbd3353121b093e8137625486
flip <ansuz@transitiontech.ca>
2014-10-15 18:09:56 -0400

    server automagically binds to your cjdns ip

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var bot = require("./lib/hiabot.js");
 
 bot.init(); // init the bot, duh
+var myfc = require("./lib/findMyFC.js")()[0];
 
 var express = require("express"),
     app = express(),
     options = {
       debug:true
-      ,address:""
+      ,address:myfc
       ,port:8086
       ,alert:function(){console.log("Server listening on %s:%s",options.address,options.port)}
     };
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname+"/public/index.html");
+  
 });
 
 app.listen(options.port, options.address, options.alert);
