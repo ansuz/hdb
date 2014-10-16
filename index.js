182dda5fcf0ac7a2f32b25ef27964e881c2a15c8
flip <ansuz@transitiontech.ca>
2014-10-16 15:56:19 -0400

    add route which naively lists peers. caching coming soon

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var bot = require("./lib/hiabot.js");
+//var bot = require("./lib/hiabot.js");
 
-bot.init(); // init the bot, duh
-var myfc = require("./lib/findMyFC.js")()[0];
+//bot.init(); // init the bot, duh
+
+var $$=require("./lib/collect.js"); // I'm really lazy
+var routes = require("./lib/routes.js");
+
+var address=$$.fixIPV6($$.myFC()[0]);
 
 var express = require("express"),
     app = express(),
     options = {
       debug:true
-      ,address:myfc
+      ,address:address
       ,port:8086
-      ,alert:function(){console.log("Server listening on %s:%s",options.address,options.port)}
+      ,alert:function(){console.log("Server listening on http://[%s]:%s",options.address,options.port)}
     };
 
-app.get("/", function (req, res) {
+// lists all peers, ignoring current status
+app.get(/^\/peers/,routes.peers); 
+
+app.get(/.*/, function (req, res) { // all get calls
   res.sendFile(__dirname+"/public/index.html");
-  
+});
+
+app.post(/.*/,function(req,res){ // all post calls
+  var posted =  JSON.parse(req.body);
+  console.log(posted);
+  res.send("hey thanks bud");
 });
 
 app.listen(options.port, options.address, options.alert);
