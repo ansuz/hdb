842993f454182025f36eccff76ac6051645c3456
ansuz <ansuz@transitiontech.ca>
2015-09-29 09:34:27 -0400

    server.js : use api

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var unmon=require("unmon");
 var http=require("http");
 var hdb=require("./index");
 
-var server=http.createServer(hdb.getPeers);
-
 var address=hdb.myfc()[0];
 var port=0xfc00;
+
+// load all routes from the hdb api
+Object.keys(hdb.api)
+    .forEach(function(key){
+        unmon.route(new RegExp('^/'+key+'$'),hdb.api[key]);
+    });
+
+var server=http.createServer(unmon.makeRouter(unmon.routes));
 
 server.listen(port,address,function(){
     console.log("Server listening on http://[%s]:%s",address,port);
