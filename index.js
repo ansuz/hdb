35ca2b2d06542e6d9b37163a0846b2ff76a235e9
ansuz <ansuz@transitiontech.ca>
2015-09-29 08:56:09 -0400

    index.js : this should be the library, not the server

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var agml=require("agml");
 var kad =require("kad");
 var cjdns=require("cjdnsjs");
-var unmon=require("unmon");
-var http=require("http");
 
-var server=http.createServer(function(req,res){
+function getPeers(req,res){
     cjdns.peerStats(function(peers){
         res.setHeader('Content-Type','application/json');
         res.end(JSON.stringify(peers,null,2));
     });
-});
+};
 
-server.listen(8004,cjdns.myfc()[0]);
+module.exports={
+    getPeers:getPeers,
+    myfc:cjdns.myfc,
+};
