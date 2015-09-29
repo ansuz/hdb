d4e723ecb6d0ba8f94f4d9871ed5bc123f5228be
ansuz <ansuz@transitiontech.ca>
2015-09-29 09:34:18 -0400

    index.js : export api

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var agml=require("agml");
 var kad =require("kad");
 var cjdns=require("cjdnsjs");
 
+var hdb={
+
+};
+
+hdb.page=function(){/*<!DOCTYPE html>
+<html>
+<body>
+<ul>
+*/}.toString().slice(14,-3);
+
+function index(req,res){
+    res.setHeader('Content-Type','text/html;charset=UTF-8');
+    res.end(hdb.page);
+};
+
 function getPeers(req,res){
     cjdns.peerStats(function(peers){
         res.setHeader('Content-Type','application/json');
@@ -9,7 +24,19 @@ function getPeers(req,res){
     });
 };
 
-module.exports={
+var api={
+    "":index,
+    index:index,
     getPeers:getPeers,
+};
+
+Object.keys(api)
+    .filter(function(x){return x;})
+    .forEach(function(key){
+        hdb.page+='<li><a href="/'+key+'">'+key+'</a></li>\n';
+    });
+
+module.exports={
+    api:api,
     myfc:cjdns.myfc,
 };
