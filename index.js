44f35efe28d3c70566a9dda6337f4379ea2daee0
ansuz <ansuz@transitiontech.ca>
2015-10-03 13:13:23 -0400

    index.js : background processes, new api endpoints

--- index.js
+++ index.js
@@ -2,24 +2,53 @@ var agml=require("agml");
 var kad =require("kad");
 var cjdns=require("cjdnsjs");
 
+var lib=require("./lib/"),
+    processes=lib.processes,
+    todo=lib.todo,
+    memory=lib.memory;
+
+var census=processes.census,
+    scrape=processes.ping;
+
+var level=require("level");
+
+var remember=memory(level("./test.db"));
+
+remember.on('newNode',function(ip){
+    console.log("[%s] Found new ip: [%s]", new Date().toISOString(),ip);
+});
+
+process.on('uncaughtException',function(e){
+    console.log(e);
+});
+
+var cancel=processes.census.schedulePoll(function(result){
+//    console.log(Object.keys(result));
+    remember.ips(result.ips,function(all){
+        // done callback?
+    });
+//    remember.node(result);
+//    console.log(result.nodes);
+    result.ips.forEach(function(ip){
+        result.nodes[ip].lastSeen=result.date;
+        remember.node(result.nodes[ip]);
+    });
+},5000);
+
 var hdb={
     myfc:cjdns.myfc
 };
 
+var fc=cjdns.myfc()[0];
+
 try{
-    hdb.nodeinfojson=require("./.nodeinfo.json");
+    hdb.nodeinfojson=require("./public/.nodeinfo.json");
 }catch(err){
     console.log(err);
     hdb.nodeinfojson={};
 }
 
 // helpers
-
-hdb.page=function(){/*<!DOCTYPE html>
-<html>
-<body>
-<ul>
-*/}.toString().slice(14,-3);
 
 // set the content header to json
 function asJSON(res){
@@ -35,9 +64,32 @@ function pretty(res,f){
 
 // actual routes here
 
-function index(req,res){
+function index(req,res,next){
     res.setHeader('Content-Type','text/html;charset=UTF-8');
-    res.end(hdb.page);
+    fs.readFile('./public/index.html','utf-8',function(e,out){
+        if(e){
+            res.statusCode=404;
+            next();
+        }else{
+            Object.keys(api)
+                .filter(function(x){return x;})
+                .forEach(function(key){
+                    out+='\t<li><a href="/'+key+'">'+key+'</a></li>\n';
+                });
+            res.end(out+'</ul>');
+        }
+    });
+    return;
+};
+
+function endPoints(req,res){
+    asJSON(res);
+    var result={endpoints:[]};
+    Object.keys(hdb.api)
+        .forEach(function(end){
+            result.endpoints.push('http://['+fc+']:64512/'+end);
+        });
+    res.end(JSON.stringify(result,null,2));
 };
 
 function nodeinfojson(req,res){
@@ -55,18 +107,44 @@ function dumpTable(req,res){
     pretty(res,cjdns.dumpTable);
 };
 
+function getNode(req,res){
+    // see if the url resembles '/getNode/<ip>'
+    var url=req.url;
+    var parts=req.url.split('/');
+    var ip=/fc[0-9a-f:]{10,37}/.test(parts[2])?
+        cjdns.zeropad(parts[2]):
+        cjdns.zeropad(req.connection.remoteAddress);
+
+    remember.getNode(ip,function(result){
+        asJSON(res);
+        result.ip=ip;
+        res.end(JSON.stringify(result,null,2));    
+    });
+};
+
+function knownNodes(req,res){
+    asJSON(res);
+    pretty(res,remember.knownNodes);
+};
+
+function nodeCount(req,res){
+    asJSON(res);
+    remember.knownNodes(function(result){
+        res.end(JSON.stringify({
+            nodeCount:result.length
+        },null,2));
+    });
+};
+
 var api=hdb.api={
-    "":index,
     index:index,
     peerStats:peerStats,
     dumpTable:dumpTable,
     'nodeinfo.json':nodeinfojson,
+    getNode:getNode,
+    knownNodes:knownNodes,
+    nodeCount:nodeCount,
+    endPoints:endPoints,
 };
-
-Object.keys(api)
-    .filter(function(x){return x;})
-    .forEach(function(key){
-        hdb.page+='<li><a href="/'+key+'">'+key+'</a></li>\n';
-    });
 
 module.exports=hdb;
